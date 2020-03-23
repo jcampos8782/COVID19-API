aa1310bcf9efd96225d85474ac7a76fa5d7e5464
Jason Campos <jcampos8782@gmail.com>
2020-03-23 13:02:56 -0700

    Scrub data a bit to make it more digestible. Remove county-level data from regional

--- init/mongo-init/main.py
+++ init/mongo-init/main.py
@@ -23,6 +23,7 @@ from datetime import datetime
 import hashlib
 from os import environ
 from itertools import islice
+from collections import namedtuple
 
 DB_USER = environ.get("DB_USER")
 DB_PASS = environ.get("DB_PASS")
@@ -31,47 +32,52 @@ DB_PORT = environ.get("DB_PORT",  "27017")
 DB_NAME = environ.get("DB_NAME", "cvd19")
 DB_COLLECTION = "cases"
 
-(DB_USER and DB_PASS) or exit("DB_USER and DB_PASS must be set in environment")
+DataSource = namedtuple("DataSource", "collection file")
+DATA_FILEPATH = "data/cases"
+META_FILEPATH = "data/meta"
+DATA_COLLECTIONS = ["confirmed", "deaths", "recovered"]
+META_COLLECTIONS = ["municipalities", "regions"]
+DATA = [DataSource(c, format("%s/%s.csv" % (DATA_FILEPATH, c))) for c in DATA_COLLECTIONS]
+META = [DataSource(c, format("%s/%s.csv" % (META_FILEPATH, c))) for c in META_COLLECTIONS]
 
+# Ensure user and password are set before trying to connect to mongo
+(DB_USER and DB_PASS) or exit("DB_USER and DB_PASS must be set in environment")
 client = pymongo.MongoClient("mongodb://%s:%s@%s:%s" % (DB_USER, DB_PASS, DB_HOST, DB_PORT))
 db = client[DB_NAME]
 
-for collection in db.list_collection_names():
+# Drop the collections prior to starting
+for collection in META_COLLECTIONS + ["cases"]:
     print("Dropping collection %s" % collection)
     db.drop_collection(collection)
 
-files = ['Confirmed', 'Deaths', 'Recovered']
-documents = {}
-regions = {}
-municipalities = {}
+cases = {}
+meta = {s.collection: {} for s in META}
 
-for suffix in files:
-    filename = format("time_series-ncov-%s.csv" % suffix)
-    print("Processing CSV %s" % filename)
+# Create the meta collections for regions and municipalities. Store the object ids for reference from the
+# cases collection
+for source in META:
+    print("Processing metadata file %s" % source.file)
+    with open(source.file) as file:
+        for row in csv.reader(file):
+            result = db[source.collection].insert_one({"name": row[0]})
+            meta[source.collection][row[0]] = result.inserted_id
 
-    with open(filename) as file:
-        for municipality, region, lat, lon, date, count  in islice(csv.reader(file), 2, None):
+# Process the data files.
+for source in DATA:
+    print("Processing data file %s" % source.file)
+    with open(source.file) as file:
+        for municipality, region, lat, lon, date, count in islice(csv.reader(file), 2, None):
 
-            # key composed of municipality, region, and date
+            # key composed of municipality, region, and date. All data for this key (confirmed, deaths, recovered)
+            # Will be stored in a single document
             key = hashlib.md5(format("%s_%s_%s" % (municipality, region, date)).encode()).hexdigest()
             region_id, municipality_id = None, None
 
-            if region and region in regions:
-                region_id = regions[region]
-            elif region:
-                result = db["regions"].insert_one({'name': region})
-                region_id = result.inserted_id
-                regions[region] = region_id
+            region_id = meta['regions'][region] if region else None
+            municipality_id = meta['municipalities'][municipality] if municipality else None
 
-            if municipality and municipality in municipalities:
-                municipality_id = municipalities[municipality]
-            elif municipality:
-                result = db["municipalities"].insert_one({'name': municipality})
-                municipality_id = result.inserted_id
-                municipalities[municipality] = municipality_id
-
-            if key in documents:
-                document = documents[key]
+            if key in cases:
+                document = cases[key]
             else:
                 document = {
                     "date": datetime.strptime(date, "%Y-%m-%d"),
@@ -87,15 +93,18 @@ for suffix in files:
                         "region_id": region_id
                     }
                 }
-                documents[key] = document
+                cases[key] = document
 
-            documents[key]["cases"][suffix.lower()] = int(count) if count else 0
+            cases[key]["cases"][source.collection] = int(count) if count else 0
 
 print("Creating collection %s" % DB_COLLECTION)
-for document in documents.values():
-    db["cases"].insert_one(document)
+for case in cases.values():
+    db["cases"].insert_one(case)
 
 print("Creating geolocation index")
 db["cases"].create_index([("geo", pymongo.GEOSPHERE)])
+
+print("Creating date index")
+db["cases"].create_index([("date", pymongo.DESCENDING)])
 
 print("Import complete!")
