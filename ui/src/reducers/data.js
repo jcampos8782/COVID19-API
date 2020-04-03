f0f6e52572d6348640441c1ed28c980024dab2fa
Jason Campos <jcampos8782@gmail.com>
2020-04-03 10:38:01 -0700

    Consistent ordering of series

--- ui/src/reducers/data.js
+++ ui/src/reducers/data.js
@@ -7,7 +7,15 @@ import {
 export default (state = [], action) => {
     switch(action.type) {
         case RECEIVE_SERIES:
-            return action.series.data;
+            return action.series.data.map(series => {
+              return {
+                regions: series.regions,
+                data: Object.keys(series.data).sort().reduce((obj,key) => {
+                  obj[key] = series.data[key];
+                  return obj;
+                }, {})
+              };
+            });
         // Clear data when a new region is requested.
         case REQUEST_SERIES_BY_REGION_ID:
         case REQUEST_SERIES_BY_GEOLOCATION:
