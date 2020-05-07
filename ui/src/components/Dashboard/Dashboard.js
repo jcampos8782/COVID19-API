ec3efb302f14e586aeb6991577d7222e84582b45
Jason Campos <jcampos8782@gmail.com>
2020-05-07 09:19:33 -0700

    Remove history tab

--- ui/src/components/Dashboard/Dashboard.js
+++ ui/src/components/Dashboard/Dashboard.js
@@ -42,8 +42,7 @@ export default class Dashboard extends React.Component {
                   scrollButtons="on"
                   >
                   <Tab label="Summary" {...a11yProps(0)} />
-                  <Tab label="History" {...a11yProps(1)} />
-                  <Tab label="Data" {...a11yProps(2)} />
+                  <Tab label="Data" {...a11yProps(1)} />
                 </Tabs>
               </Grid>
               <Grid item xs={12}>
